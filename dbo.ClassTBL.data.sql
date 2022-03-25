b1881aa047592445363174e2d835a069010eb97e
Kip Minifie <kip.minifie@gmail.com>
2022-03-25 21:57:39 +1100

    This Commit is the last one for the prototype assignment.

---
+++ dbo.ClassTBL.data.sql
@@ -0,0 +1,4 @@
+﻿SET IDENTITY_INSERT [dbo].[ClassTBL] ON
+INSERT INTO [dbo].[ClassTBL] ([ClassId], [ClassName]) VALUES (1, N'Digital Solutions Fr')
+INSERT INTO [dbo].[ClassTBL] ([ClassId], [ClassName]) VALUES (2, N'Digital Solutions Ba')
+SET IDENTITY_INSERT [dbo].[ClassTBL] OFF
