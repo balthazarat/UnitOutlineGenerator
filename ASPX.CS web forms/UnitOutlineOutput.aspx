a650909b0dcd5635c91e953c8aad1b8298a22239
Kip Minifie <kip.minifie@gmail.com>
2022-05-06 12:44:41 +1000

    should output data

---
+++ ASPX.CS web forms/UnitOutlineOutput.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="UnitOutlineOutput.aspx.cs" Inherits="WebApplication1.WebForm2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <br /><table id="tableContent" border="1" runat="server"></table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASPX.CS web forms/UnitOutlineOutput.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace WebApplication1
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\dbTest.mdf;Integrated Security=True";
+
+            SqlConnection con = new SqlConnection(connectionString);
+
+            SqlCommand cmd = new SqlCommand("dbo.unitOut", con);
+
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("ID", 1);
+
+            con.Open();
+
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+
+            {
+
+                HtmlTableRow row = new HtmlTableRow();
+
+                HtmlTableCell cell = new HtmlTableCell();
+
+                // this array helps us make everything fairly optimised...
+
+                string[] headers = { "Course", "Unit", "Unit Description", "Accreditation", "Unit Goals", "Content Descriptors" };
+
+                for (int i = 0; i < 6; i++)
+
+                {
+
+                    // We are bolding our headings with the <b> tag, adding a break, then adding the content under
+
+                    //We are getting the heading info from the array and the data from our datareader
+
+                    cell.InnerHtml = string.Format("<b>{0}</b><br />{1}", headers[i], dr[headers[i]]);
+
+                    row.Cells.Add(cell);
+
+                    tableContent.Rows.Add(row);
+
+                    cell = new HtmlTableCell();
+
+                    row = new HtmlTableRow();
+                }
+                con.Close();
+            }
+        }
+    }
+}
