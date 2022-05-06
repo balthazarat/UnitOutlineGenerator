ded743da4900cc23b082f31bea77f1312492ee4a
Kip Minifie <kip.minifie@gmail.com>
2022-05-06 11:21:20 +1000

    upload off VS

---
+++ SQLQuery2.sql
@@ -0,0 +1,11 @@
+﻿-- copy table data --
+	  -- course --
+	UPDATE CourseTBL
+	SET CourseTBL.Course = OutlineDataTBL.Course
+	FROM OutlineDataTBL, CourseTBL
+	--WHERE OutlineDataTBL.Id = CourseTBL.Id
+	  -- unit --
+	UPDATE UnitTBL
+	SET UnitTBL.Unit = OutlineDataTBL.Unit
+	FROM OutlineDataTBL, UnitTBL
+	--WHERE OutlineDataTBL.Id = UnitTBL.Id
---
+++ importCSVtoTBL.sql
@@ -0,0 +1,9 @@
+﻿BULK INSERT OutlineDataTBL -- The table we want to put the CSV in
+FROM 'C:\Users\Administrator\Downloads\courseData.csv' -- Location of the CSV file
+WITH
+(
+    FIRSTROW = 2, -- as 1st one is header
+    FIELDTERMINATOR = '|',  --CSV field delimiter
+    ROWTERMINATOR = '\n',   --Use to shift the control to next row
+    TABLOCK
+)
